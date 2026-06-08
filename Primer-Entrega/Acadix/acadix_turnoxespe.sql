1a0a28166b0e7aca95160868e9f6bcfa01c5ded8
ezequiel <ezequiel.toriaci.417@alu.tecnica29de6.edu.ar>
2026-06-07 23:14:35 -0300

    Add files via upload

---
+++ Primer-Entrega/Acadix/acadix_turnoxespe.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 8.0.43, for Win64 (x86_64)
+--
+-- Host: localhost    Database: acadix
+-- ------------------------------------------------------
+-- Server version	8.0.43
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `turnoxespe`
+--
+
+DROP TABLE IF EXISTS `turnoxespe`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `turnoxespe` (
+  `id_TxE` int NOT NULL AUTO_INCREMENT,
+  `ID_turno` int DEFAULT NULL,
+  `ID_espe` int DEFAULT NULL,
+  PRIMARY KEY (`id_TxE`),
+  KEY `ID_turno_idx` (`ID_turno`),
+  KEY `ID_espe_idx` (`ID_espe`),
+  CONSTRAINT `ID_espe` FOREIGN KEY (`ID_espe`) REFERENCES `especialidad` (`id_espe`),
+  CONSTRAINT `ID_turno` FOREIGN KEY (`ID_turno`) REFERENCES `turno` (`id_turno`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `turnoxespe`
+--
+
+LOCK TABLES `turnoxespe` WRITE;
+/*!40000 ALTER TABLE `turnoxespe` DISABLE KEYS */;
+/*!40000 ALTER TABLE `turnoxespe` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-06-07 22:49:58
